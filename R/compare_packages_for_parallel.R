bacd36980489dd318a7de9d455085c3e969f4511
Mun-Gwan Hong <mungwan.hong@nbis.se>
2021-05-30 09:49:00 +0200

    A new function for comparing packages for parallel computing

---
+++ R/compare_packages_for_parallel.R
@@ -0,0 +1,59 @@
+# ------------------
+#' Compare packages for parallel computing
+#'
+#' Surprisingly, performance of the packages for parallel computing differs
+#' substantially. This requires a lot of those packages to be loaded.
+#'
+#' @export
+#'
+#' @examples
+#' compare_packages_for_parallel()
+# ------------------
+
+compare_packages_for_parallel <- function() {
+  n_cores <- parallel::detectCores() - 1
+
+  microbenchmark::microbenchmark(
+    parallel = {
+      cl <- parallel::makeCluster(n_cores)
+      doParallel::registerDoParallel(cl)
+      foreach(
+        ii = iterators::icount(1000),
+        .inorder = FALSE
+      ) %dopar% {
+        lm(Sepal.Length ~ Species, iris)
+      }
+      parallel::stopCluster(cl)
+    },
+    parallel2 = {
+      doParallel::registerDoParallel(cores = n_cores)
+      foreach(
+        ii = iterators::icount(1000),
+        .inorder = FALSE
+      ) %dopar% {
+        lm(Sepal.Length ~ Species, iris)
+      }
+    },
+    doSNOW = {
+      cl <- snow::makeCluster(n_cores)
+      doSNOW::registerDoSNOW(cl)
+      foreach(
+        ii = iterators::icount(1000),
+        .inorder = FALSE
+      ) %dopar% {
+        lm(Sepal.Length ~ Species, iris)
+      }
+      snow::stopCluster(cl)
+    },
+    doMC = {
+      doMC::registerDoMC(n_cores)
+      foreach(
+        ii = iterators::icount(1000),
+        .inorder = FALSE
+      ) %dopar% {
+        lm(Sepal.Length ~ Species, iris)
+      }
+    },
+    times = 10L
+  )
+}
